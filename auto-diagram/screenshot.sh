4c01c9870b34aee4becce048ce8134fe67da59e3
rohitmusti <rohit.musti.rm@gmail.com>
2018-07-18 17:34:40 -0400

    saving the work

--- auto-diagram/screenshot.sh
+++ auto-diagram/screenshot.sh
@@ -5,5 +5,5 @@ sleep 10 && cd ./headless_chrome && google-chrome-stable --headless --disable-gp
 sleep 20 && node ./headless_chrome/screenshot2.js
 mv ./headless_chrome/output.jpeg ./output.jpeg
 killall npm -v
-killall -v -y 20s chrome 
+killall -v -y 30s chrome 
 echo "press ctrl+c to exit"
