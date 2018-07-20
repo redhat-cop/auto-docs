e1ed2767111d86344ff14316df041cd4cdfd7b34
rohitmusti <rohit.musti.rm@gmail.com>
2018-07-20 09:48:46 -0400

    sort of working, still cleaning up

--- auto-diagram/screenshot.sh
+++ auto-diagram/screenshot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 python3 commandline.py
 npm run serve &
-sleep 10 && cd ./headless_chrome && google-chrome-stable --headless --disable-gpu --hide-scrollbars --remote-debugging-port=9222 http://localhost:8080/ &
+sleep 10 && cd ./headless_chrome && google-chrome-stable --no-sandbox --headless --disable-gpu --hide-scrollbars --remote-debugging-port=9222 http://localhost:8080/ &
 sleep 20 && node ./headless_chrome/screenshot2.js
 mv ./headless_chrome/output.jpeg ./output.jpeg
 killall npm -v
