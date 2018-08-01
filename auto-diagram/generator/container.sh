2c3909f081e8b482ce0054b8037ef3e39978eaa4
rohitmusti <rohit.musti.rm@gmail.com>
2018-08-01 09:43:34 -0400

    got it working

--- auto-diagram/generator/container.sh
+++ auto-diagram/generator/container.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 npm run serve &
 sleep 10 && cd ./headless_chrome && google-chrome-stable --no-sandbox --headless --disable-gpu --hide-scrollbars --remote-debugging-port=9222 http://localhost:8080/ &
 sleep 20 && node ./headless_chrome/screenshot2.js
